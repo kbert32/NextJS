d88e6812a3c3045fb6ff60bf73e3aae9770e3d93
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-05-16 20:57:40 -0600

    10

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+import NotificationContext from "@/store/notification-context";
 
 export default function Layout(props) {
+  useContext();
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      <Notification title="Test" message="This is a test." status="pending" />
     </>
   );
 }
