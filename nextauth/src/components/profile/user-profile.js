54d7e4fd20012e10bf6ef5e60dbcb79c9029086f
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-05-26 10:23:09 -0600

    13

--- nextauth/src/components/profile/user-profile.js
+++ nextauth/src/components/profile/user-profile.js
@@ -2,12 +2,27 @@ import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
 
 function UserProfile() {
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
 
 export default UserProfile;
+
+//$2a$12$8emkX5hsHaVZVYMEzRpCYuT4QsQkfSK0VZSTR7l55vadffnzXD6T2
